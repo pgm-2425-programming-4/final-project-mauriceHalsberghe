9cd7a6bafcaf52b91a85b1ff3e360b0af3bd9727
mauriceHalsberghe <maurice.halsberghe@gmail.com>
2025-06-10 09:08:24 +0200

    Refactor main.jsx and routes to improve code structure and add project fetching functionality

--- client/src/routes/__root.jsx
+++ client/src/routes/__root.jsx
@@ -1,16 +1,36 @@
 import { Outlet, createRootRoute, Link } from '@tanstack/react-router'
+import { fetchProjects } from "../queries/fetch-projects";
+import { useQuery } from "@tanstack/react-query";
 
 export const Route = createRootRoute({
-  component: () => (
-    <>
-      <header className='header'>
-        <h1>Jammin'</h1>
-        <nav className='navigation'>
-            <Link to="">Home</Link>
-            <Link to="/backlog">Backlog</Link>
-        </nav>
-      </header>
-        <Outlet />
-    </>
-  ),
+  component: Navigation,
+
 })
+
+function Navigation() {
+    const { data, isLoading, error } = useQuery({
+      queryKey: ["users"],
+      queryFn: fetchProjects,
+    });
+
+    if (isLoading) return <p>Loading...</p>;
+    if (error) return <p>Error: {error.message}</p>;
+  return (
+    <>
+        <nav className='navigation'>
+            <Link to="/">Home</Link>
+              <ul className="aside__list">
+              {data.map((project) => (
+                <li className="aside__item" key={project.id}>
+                  <Link to={`/projects/${project.id}`} className="[&.active]:font-bold">
+                    {project.title}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+            <Link to="/about">About</Link>
+        </nav>
+        <Outlet/>
+    </>
+  )
+}
--- client/src/routes/index.jsx
+++ client/src/routes/index.jsx
@@ -1,31 +1,11 @@
-import { Link, createFileRoute } from "@tanstack/react-router";
-import { fetchProjects } from "../queries/fetch-projects";
-import { useQuery } from "@tanstack/react-query";
+import { createFileRoute } from "@tanstack/react-router";
+
 
 export const Route = createFileRoute("/")({
-  component: Users,
+  component: () => {
+    <div>
+      test
+    </div>
+  }
 });
 
-function Users() {
-  const { data, isLoading, error } = useQuery({
-    queryKey: ["users"],
-    queryFn: fetchProjects,
-  });
-
-  if (isLoading) return <p>Loading...</p>;
-  if (error) return <p>Error: {error.message}</p>;
-  
-  return (
-    <div className="aside"> 
-      <ul className="aside__list">
-        {data.map((project) => (
-          <li className="aside__item" key={project.id}>
-            <Link to={`/projects/${project.id}`} className="[&.active]:font-bold">
-              {project.title}
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-}
