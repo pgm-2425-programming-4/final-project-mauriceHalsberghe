7db990ba226f11b23c38d485672fa72c4168bece
mauriceHalsberghe <maurice.halsberghe@gmail.com>
2025-06-10 09:19:40 +0200

    Added basic content to homepage

--- client/src/routes/about/index.jsx
+++ client/src/routes/about/index.jsx
@@ -1,10 +1,10 @@
 import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/about/')({
-  component: RouteComponent,
+  component: About,
 })
 
-function RouteComponent() {
+function About() {
   return (
     <div>
       <p>
--- client/src/routes/index.jsx
+++ client/src/routes/index.jsx
@@ -1,11 +1,15 @@
 import { createFileRoute } from "@tanstack/react-router";
 
+export const Route = createFileRoute('/')({
+  component: Home,
+})
 
-export const Route = createFileRoute("/")({
-  component: () => {
+function Home() {
+  return (
     <div>
-      test
-    </div>
-  }
-});
-
+      <p>
+        Welcome to my Kanban app! Please select an item from the navigation menu.
+      </p>
+    </div> 
+  )
+}
