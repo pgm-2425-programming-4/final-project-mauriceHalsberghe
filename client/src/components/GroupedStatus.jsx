9db10e9e1241b4f5d45d875d7df71ea529851564
mauriceHalsberghe <maurice.halsberghe@gmail.com>
2025-06-16 01:55:15 +0200

    Added backlog tasks not showing on homepage

--- client/src/components/GroupedStatus.jsx
+++ client/src/components/GroupedStatus.jsx
@@ -1,27 +1,30 @@
-import React from 'react';
+import React from "react";
 
 export default function GroupedStatus({ tasks, statuses }) {
-
   const groupedByStatus = groupTasksByStatus(tasks);
-  
-  return (
-    <div className='main__content'>
-      {statuses.map(status => (
-        <div className='status' key={status.id}>
-          <h3 className='status__title'>{status.name}</h3>
-          <ul className='status__list'>
-            {(groupedByStatus[status.id] || []).map(task => (
-              <li className='status__card' key={task.id}>{task.title}</li>
-            ))}
-          </ul>
-        </div>
-      ))}
 
-      {groupedByStatus['no-status'] && (
+  return (
+    <div className="main__content">
+      {statuses
+        .filter((status) => status.name.toLowerCase() !== "backlog")
+        .map((status) => (
+          <div className="status" key={status.id}>
+            <h3 className="status__title">{status.name}</h3>
+            <ul className="status__list">
+              {(groupedByStatus[status.id] || []).map((task) => (
+                <li className="status__card" key={task.id}>
+                  {task.title}
+                </li>
+              ))}
+            </ul>
+          </div>
+        ))}
+
+      {groupedByStatus["no-status"] && (
         <div>
           <h3>No Status</h3>
           <ul>
-            {groupedByStatus['no-status'].map(task => (
+            {groupedByStatus["no-status"].map((task) => (
               <li key={task.id}>{task.title}</li>
             ))}
           </ul>
@@ -34,8 +37,8 @@ export default function GroupedStatus({ tasks, statuses }) {
 function groupTasksByStatus(tasks) {
   const grouped = {};
 
-  tasks.forEach(task => {
-    const statusId = task.task_status?.id || 'no-status';
+  tasks.forEach((task) => {
+    const statusId = task.task_status?.id || "no-status";
     if (!grouped[statusId]) {
       grouped[statusId] = [];
     }
