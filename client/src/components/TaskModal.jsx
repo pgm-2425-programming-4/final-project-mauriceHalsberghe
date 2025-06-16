ae28bb3bf0fb92c88e2e404e685423e81fab73eb
mauriceHalsberghe <maurice.halsberghe@gmail.com>
2025-06-16 21:27:06 +0200

    Added modal styling

--- client/src/components/TaskModal.jsx
+++ client/src/components/TaskModal.jsx
@@ -1,13 +1,29 @@
 import React from "react";
 
 export function TaskModal({ task }) {
-  if(!task) {
-    return
+  if (!task) {
+    return null;
   }
 
   return (
-    <div>
-      <h2>{task.title}</h2>
+    <div className="modal-bg">
+      <div className="modal">
+        <div>
+          <label>Title</label>
+          <input value={task.title} />
+        </div>
+
+        <div>
+          <label>Description</label>
+          <input value={task.description} />
+        </div>
+
+        <div>
+          <label>Status</label>
+          <opiton></opiton>
+          <select></select>
+        </div>
+      </div>
     </div>
   );
 }
