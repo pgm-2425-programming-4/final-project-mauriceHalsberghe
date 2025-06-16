2fed7d3fde22075311de2a83a1b05244b94f3d0b
mauriceHalsberghe <maurice.halsberghe@gmail.com>
2025-06-16 12:07:46 +0200

    added pagination to backlog page

--- client/src/routes/projects/$projectId/backlog.jsx
+++ client/src/routes/projects/$projectId/backlog.jsx
@@ -27,23 +27,37 @@ function BacklogPage() {
   const project = tasks.length > 0 ? tasks[0].project : null;
 
   const [currentPage, setCurrentPage] = useState(1);
-  const [pageSize, setPageSize] = useState(5);
+  const [pageSize, setPageSize] = useState(10);
   const pageCount = Math.ceil(backlogTasks.length / pageSize);
+
+  const paginatedTasks = backlogTasks.slice(
+    (currentPage - 1) * pageSize,
+    currentPage * pageSize
+  );
 
   return (
     <section className="main backlog">
       <h2 className="backlog__title">Backlog for {project.title}</h2>
 
-      {backlogTasks.length === 0 ? (
+      {paginatedTasks.length === 0 ? (
         <p>No backlog tasks found</p>
       ) : (
+        <>
         <ul className="backlog__list">
-          {backlogTasks.map((task) => (
+          {paginatedTasks.map((task) => (
             <li className="backlog__item" key={task.id}>
               {task.title}
             </li>
           ))}
         </ul>
+
+        <Pagination 
+          currentPage={currentPage} 
+          pageSize={pageSize} 
+          onPageChanged={(page) => setCurrentPage(page)} 
+          pageCount={pageCount}
+          onPageSizeChanged={(size) => {setPageSize(size); setCurrentPage(1);}} />
+        </>
       )}
     </section>
   );
