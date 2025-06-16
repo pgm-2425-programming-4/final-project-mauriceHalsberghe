5a0bf908c36f72f1bdc5e8a67b6cbab59930b965
mauriceHalsberghe <maurice.halsberghe@gmail.com>
2025-06-16 12:01:07 +0200

    imported existing pagination component in backlog

--- client/src/routes/projects/$projectId/backlog.jsx
+++ client/src/routes/projects/$projectId/backlog.jsx
@@ -1,11 +1,11 @@
-import { createFileRoute, notFound } from '@tanstack/react-router';
-import { fetchTasks } from '../../../queries/fetch-tasks-by-projectid';
+import { createFileRoute, notFound } from "@tanstack/react-router";
+import { fetchTasks } from "../../../queries/fetch-tasks-by-projectid";
+import React, { useState } from "react";
+import { Pagination } from "../../../components/Pagination";
 
-export const Route = createFileRoute('/projects/$projectId/backlog')({
+export const Route = createFileRoute("/projects/$projectId/backlog")({
   loader: async ({ params }) => {
-    const [tasks] = await Promise.all([
-      fetchTasks(params.projectId),
-    ]);
+    const [tasks] = await Promise.all([fetchTasks(params.projectId)]);
 
     if (!tasks || tasks.length === 0) {
       throw notFound();
@@ -15,26 +15,33 @@ export const Route = createFileRoute('/projects/$projectId/backlog')({
   },
 
   component: BacklogPage,
-  notFoundComponent: () => <div>Project not found</div>
+  notFoundComponent: () => <div>Project not found</div>,
 });
 
 function BacklogPage() {
   const { tasks } = Route.useLoaderData();
-  
+
   const backlogTasks = tasks.filter(
-    task => task.task_status.name === 'Backlog'
+    (task) => task.task_status.name === "Backlog"
   );
   const project = tasks.length > 0 ? tasks[0].project : null;
 
-  return (
-    <section className='main backlog'>
-      <h2 className='backlog__title'>Backlog for { project.title }</h2>
+  const [currentPage, setCurrentPage] = useState(1);
+  const [pageSize, setPageSize] = useState(5);
+  const pageCount = Math.ceil(backlogTasks.length / pageSize);
 
-      {backlogTasks.length === 0 ? ( <p>No backlog tasks found</p>) : 
-      (
-        <ul className='backlog__list'>
-          {backlogTasks.map(task => (
-            <li className='backlog__item' key={task.id}>{task.title}</li>
+  return (
+    <section className="main backlog">
+      <h2 className="backlog__title">Backlog for {project.title}</h2>
+
+      {backlogTasks.length === 0 ? (
+        <p>No backlog tasks found</p>
+      ) : (
+        <ul className="backlog__list">
+          {backlogTasks.map((task) => (
+            <li className="backlog__item" key={task.id}>
+              {task.title}
+            </li>
           ))}
         </ul>
       )}
