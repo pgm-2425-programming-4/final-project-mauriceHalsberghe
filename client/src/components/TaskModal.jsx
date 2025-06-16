74dd624ecc0d84ab7d87dc8f32b86524afa0ca00
mauriceHalsberghe <maurice.halsberghe@gmail.com>
2025-06-16 22:39:18 +0200

    Updated modal visuality

--- client/src/components/TaskModal.jsx
+++ client/src/components/TaskModal.jsx
@@ -1,27 +1,38 @@
 import React from "react";
 
-export function TaskModal({ task }) {
+export function TaskModal({ task, onClose }) {
   if (!task) {
     return null;
   }
 
   return (
-    <div className="modal-bg">
-      <div className="modal">
-        <div>
-          <label>Title</label>
-          <input value={task.title} />
-        </div>
+    <div className="modal">
+      <div className="modal-bg" onClick={onClose}></div>
 
-        <div>
-          <label>Description</label>
-          <input value={task.description} />
-        </div>
+      <div className="modal__card">
+        <button className="button button--close" onClick={onClose}>
+          <img src="/xmark-solid.svg" />
+        </button>
 
-        <div>
-          <label>Status</label>
-          <opiton></opiton>
-          <select></select>
+        <div className="modal__content">
+          <div>
+            <label>Title</label>
+            <input value={task.title} />
+          </div>
+
+          <div>
+            <label>Description</label>
+            <textarea name="" id="">{task.description}</textarea>
+          </div>
+
+          <div>
+            <label>Status</label>
+            <select>
+              <option>1</option>
+              <option>1</option>
+              <option>1</option>
+            </select>
+          </div>
         </div>
       </div>
     </div>
