1049a4d618b6021f6b541bb32bd8f55298b61a9f
mauriceHalsberghe <maurice.halsberghe@gmail.com>
2025-06-16 23:01:41 +0200

    Updated modal status select

--- client/src/components/TaskModal.jsx
+++ client/src/components/TaskModal.jsx
@@ -1,6 +1,17 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { fetchTasksStatuses } from "../queries/fetch-tasks-statuses";
 
 export function TaskModal({ task, onClose }) {
+  const [statuses, setStatuses] = useState([]);
+
+  useEffect(() => {
+    async function fetchStatuses() {
+      const data = await fetchTasksStatuses();
+      setStatuses(data);
+    }
+    fetchStatuses();
+  }, []);
+
   if (!task) {
     return null;
   }
@@ -17,20 +28,24 @@ export function TaskModal({ task, onClose }) {
         <div className="modal__content">
           <div>
             <label>Title</label>
-            <input value={task.title} />
+            <input value={task.title} readOnly />
           </div>
 
           <div>
             <label>Description</label>
-            <textarea name="" id="">{task.description}</textarea>
+            <textarea readOnly >
+              {task.description}
+            </textarea>
           </div>
 
           <div>
             <label>Status</label>
-            <select>
-              <option>1</option>
-              <option>1</option>
-              <option>1</option>
+            <select value={task.task_status?.id} readOnly>
+              {statuses.map((status) => (
+                <option key={status.id} value={status.id}>
+                  {status.name}
+                </option>
+              ))}
             </select>
           </div>
         </div>
