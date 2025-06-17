519354429abe60e5a793214f4bfd21a7ca8f3fa0
mauriceHalsberghe <maurice.halsberghe@gmail.com>
2025-06-17 10:55:41 +0200

    Added task posting query

---
+++ client/src/queries/add-task.js
@@ -0,0 +1,26 @@
+import { API_URL, API_TOKEN } from "../constants/constants";
+
+export async function addTask(statusId, data) {
+  
+  const response = await fetch(`${API_URL}/tasks`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${API_TOKEN}`,
+    },
+    body: JSON.stringify({
+      data: {
+        title: data.title,
+        description: data.description,
+        task_status: statusId,
+      }
+    }),
+  });
+  
+  if (!response.ok) {
+    const errorData = await response.json();
+    throw new Error(errorData.error?.message || "error adding task");
+  }
+
+  return await response.json();
+}
