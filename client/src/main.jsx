b996b9bf8ccef2b9c3120e4cda161341b02bf516
mauriceHalsberghe <maurice.halsberghe@gmail.com>
2025-06-17 23:48:58 +0200

    Updated main css and file structure

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
+import './main.css';
 import { RouterProvider, createRouter } from '@tanstack/react-router';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
